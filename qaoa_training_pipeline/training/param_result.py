b0508f0d4034f3660eb297a4311cf379863fe87e
Daniel Egger <deg@zurich.ibm.com>
2025-12-02 11:56:55 +0100

    * Fix for the degree in the fixed angles trainer.

--- qaoa_training_pipeline/training/param_result.py
+++ qaoa_training_pipeline/training/param_result.py
@@ -47,7 +47,7 @@ class ParamResult:
             "system": platform.system(),
             "processor": platform.processor(),
             "platform": platform.platform(),
-            "qaoa_training_pipeline_version": 20,
+            "qaoa_training_pipeline_version": 21,
         }
 
         # Convert, e.g., np.float to float
