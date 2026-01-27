e55c86d5cbf926170f4a72a1114b4047159233bc
Liran Szlak <liran.szlak@ibm.com>
2026-01-27 10:16:45 +0200

    update version

--- qaoa_training_pipeline/training/param_result.py
+++ qaoa_training_pipeline/training/param_result.py
@@ -47,7 +47,7 @@ class ParamResult:
             "system": platform.system(),
             "processor": platform.processor(),
             "platform": platform.platform(),
-            "qaoa_training_pipeline_version": 29,
+            "qaoa_training_pipeline_version": 30,
         }
 
         # Convert, e.g., np.float to float
@@ -96,7 +96,9 @@ class ParamResult:
     def from_scipy_result(cls, result, params0, train_duration, sign, trainer) -> dict:
         """Standardizes results from SciPy such that it can be serialized."""
 
-        param_result = cls(result.pop("x").tolist(), train_duration, trainer, sign * result.pop("fun"))
+        param_result = cls(
+            result.pop("x").tolist(), train_duration, trainer, sign * result.pop("fun")
+        )
 
         result = dict(result)
 
