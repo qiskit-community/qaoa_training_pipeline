aee49fcb1eca7d26d1bfa8da9ef556333a31cf94
Joel Jurado Diaz <jurado.diaz.joel@gmail.com>
2026-06-24 17:27:35 +0200

    Fix import issue

--- qaoa_training_pipeline/problem_params_provider.py
+++ qaoa_training_pipeline/problem_params_provider.py
@@ -22,7 +22,7 @@ from abc import abstractmethod
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
 
-from qaoa_training_pipeline.qaoa_training_pipeline.params_provider import ParamsProvider
+from qaoa_training_pipeline.params_provider import ParamsProvider
 from qaoa_training_pipeline.training.param_result import ParamResult
 
 
