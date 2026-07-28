b764dab0df50792b42078270e2448686f70e14fd
Joel Jurado Diaz <jurado.diaz.joel@gmail.com>
2026-07-28 15:39:02 +0200

    Fix test custom ansatz to new functionality

--- test/evaluation/test_qpu_evaluator.py
+++ test/evaluation/test_qpu_evaluator.py
@@ -87,10 +87,8 @@ class TestQPUSampleEvaluator(TestCase):
         ansatz_op = SparsePauliOp.from_list([("ZI", 1)])
 
         angles = [1.2, 1.3]
-        self.evaluator.prepare_ansatz(ansatz_op, len(angles) // 2)
         energy1 = self.evaluator.evaluate(self.cost_op, params=angles, ansatz_circuit=ansatz_op)
-        self.evaluator.prepare_ansatz(self.cost_op, len(angles) // 2)
-        energy2 = self.evaluator.evaluate(self.cost_op, params=angles)
+        energy2 = self.evaluator.evaluate(self.cost_op, params=angles, ansatz_circuit=self.cost_op)
 
         self.assertNotAlmostEqual(energy1, energy2, delta=0.1)
 
