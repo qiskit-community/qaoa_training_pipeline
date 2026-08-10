a8b9cb7e7e75e01bdfd7d8317b2339ccd13570a0
Yishai Shimoni <shimoni.yishai@gmail.com>
2026-08-10 15:46:14 +0300

    bump version to 45

--- qaoa_training_pipeline/framework/param_result.py
+++ qaoa_training_pipeline/framework/param_result.py
@@ -48,7 +48,7 @@ class ParamResult:
             "system": platform.system(),
             "processor": platform.processor(),
             "platform": platform.platform(),
-            "qaoa_training_pipeline_version": 44,
+            "qaoa_training_pipeline_version": 45,
         }
 
         # Convert, e.g., np.float to float
