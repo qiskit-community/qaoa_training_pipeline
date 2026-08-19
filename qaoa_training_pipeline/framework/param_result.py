0664aa6f4d43d818d0d7d8b4c391f1e1c95fad30
Joel Jurado Diaz <jurado.diaz.joel@gmail.com>
2026-08-19 10:26:23 +0200

    Update README and version

--- qaoa_training_pipeline/framework/param_result.py
+++ qaoa_training_pipeline/framework/param_result.py
@@ -48,7 +48,7 @@ class ParamResult:
             "system": platform.system(),
             "processor": platform.processor(),
             "platform": platform.platform(),
-            "qaoa_training_pipeline_version": 47,
+            "qaoa_training_pipeline_version": 48,
         }
 
         # Convert, e.g., np.float to float
